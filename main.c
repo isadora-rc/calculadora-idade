6b0183179bec4a37c5f6fd4e1fba76cf8f799e82
isadora-rc <isadorarochacosta@gmail.com>
2026-09-10 17:26:15 -0300

    Adiciona o cálculo da idade aproximada

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 
 int main() {
 
-    int anoNasc;
+    int anoNasc, anoAtual=2026, idade;
 
     printf("Calculadora de Idade\n");
 
     printf("Digite o ano em que você nasceu: ");
     scanf("%d", &anoNasc);
+
+    idade = anoAtual - anoNasc;
+
+    printf("Idade: %d ano.\n", idade);
 
     return 0;
 }
