eebc19afd536139204836bba7b611c27688e25b4
isadora-rc <isadorarochacosta@gmail.com>
2026-09-10 17:21:08 -0300

    Adiciona a entrada do ano de nascimento

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
+
+    int anoNasc;
+
     printf("Calculadora de Idade\n");
+
+    printf("Digite o ano em que você nasceu: ");
+    scanf("%d", &anoNasc);
 
     return 0;
 }
